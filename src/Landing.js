c43e08f4701c794f32bd758c54ffc92f430a55f7
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-05-09 16:09:00 +0200

    first commit

---
+++ src/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+import './index.css';
+import Smarthome from './images/home.png';
+
+const Landing = () => (
+  <div className="App">
+    <div className='welcome'>
+      <h1>Welcome to <span className="smarthome">Smarthome</span></h1>
+      <p>Let's manage your smart home</p>
+    </div>
+    <div className="smartH_img">
+      <img className='img' src={Smarthome}></img>
+    </div>
+
+    <div className='btnStart'>
+         <a><Button type="primary">Get Started</Button></a>
+    </div>
+  </div>
+);
+
+export default Landing;
