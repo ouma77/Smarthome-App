15f933dac0d772fe4430b123a33ca4c4de020590
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-05-10 02:35:16 +0200

    setting modification

--- src/Setting.js
+++ src/Setting.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import { Card,Avatar } from 'antd';
+import { Card, Avatar, Switch, Button } from 'antd';
 import { KeyOutlined,WindowsOutlined,AlertOutlined } from '@ant-design/icons';
 import './App.css';
 import lampe from './images/lampe.jpg';
 import temperature from './images/temperature.jpg';
 
-const style = { background: '#007bff', padding: '8px 0' };
+function onChange(checked) {
+    console.log(`switch to ${checked}`);
+}
 
 class Setting extends Component{
     render(){
@@ -16,12 +18,15 @@ class Setting extends Component{
                 type="inner">
                 <Avatar size={40} src={temperature} />
                 Température
+                <div className="onOff">Nuageux</div>
                 </Card>
                 <Card
                 style={{ marginTop: 16 }}
                 type="inner">
                 <Avatar size={40} style={{ color: '#007bff' , background:'none' }}icon={<AlertOutlined />}/>
                 Alert
+                <div className="onOff">ON/OFF <Switch size="small" defaultChecked onChange={onChange} /></div>
+                <div className="onOff">All lights OFF at : <Button type="primary" shape="round" size="small">01:00</Button></div>
                 </Card>
 
                 <Card
@@ -29,12 +34,14 @@ class Setting extends Component{
                 type="inner">
                 <Avatar size={40} src={lampe} />
                 Mouvement ligth
+                <div className="onOff">Auto <Switch size="small" defaultChecked onChange={onChange} /></div>
                 </Card>
 
                 <Card
                 style={{ marginTop: 16 }}
                 type="inner">
                 <Avatar size={40} src={lampe} />
+                <div className="onOff">ON/OFF <Switch size="small" defaultChecked onChange={onChange} /></div>
                 Light
                 </Card>
 
@@ -43,6 +50,7 @@ class Setting extends Component{
                 type="inner">
                 <Avatar size={40} style={{ color: '#007bff' , background:'none' }}icon={<KeyOutlined />}/>
                 Door System
+                <div className="onOff">Open/Close <Switch size="small" defaultChecked onChange={onChange} /></div>
                 </Card>
                 
                 <Card
@@ -50,6 +58,7 @@ class Setting extends Component{
                 type="inner">
                 <Avatar size={40} style={{ color: '#007bff' , background:'none' }}icon={<KeyOutlined />}/>
                 Door : Garage
+                <div className="onOff">Open/Close <Switch size="small" defaultChecked onChange={onChange} /></div>
                 </Card>
 
                 <Card
@@ -57,11 +66,12 @@ class Setting extends Component{
                 type="inner">
                 <Avatar size={40} style={{ color: '#007bff' , background:'none' }}icon={<WindowsOutlined />}/>
                 Window
+                <div className="onOff">Auto <Switch size="small" defaultChecked onChange={onChange} /></div>
+                <div className="onOff">All lights OFF at : <Button type="primary" shape="round" size="small">01:00</Button></div>
                 </Card>
 
             </div>
 
-            
         );
     }
 }
