4cdadc2cfff148b13ab588ae3111109c58177ea3
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-05-10 17:18:39 +0200

    start bedroom

--- src/Bedroom.js
+++ src/Bedroom.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
-import { Col, Row,Divider} from 'antd';
+import { Tabs,Select} from 'antd';
+import { StickyContainer, Sticky } from 'react-sticky';
 import './App.css';
+import lampe from './images/lampe.png';
 
-const style = { background: '#007bff', padding: '8px 0' };
+const { TabPane } = Tabs;
+
+const renderTabBar = (props, DefaultTabBar) => (
+    <Sticky>
+      {({ style }) => (
+        <DefaultTabBar {...props} className="site-custom-tab-bar" style={{ ...style }} />
+      )}
+    </Sticky>
+);
 
 class Rooms extends Component{
     render(){
         return(
             <div className="App">
-                <div className="circle">3iw</div>
+                <StickyContainer>
+                <Tabs tabPosition="bottom" defaultActiveKey="1" style={{ marginLeft: 55 }}>
+                    <TabPane tab="Temperature" key="1" style={{ height: 572 }}>
+                        <div className="circle">Temperature</div>
+                    </TabPane>
+                    <TabPane tab="Light" key="2">
+                        <div className="circle">Light</div>
+                    </TabPane>
+                    <TabPane tab="Window" key="3">
+                        <div className="circle">Window</div>
+                    </TabPane>
+                </Tabs>
+                </StickyContainer>
             </div>     
         );
     }
